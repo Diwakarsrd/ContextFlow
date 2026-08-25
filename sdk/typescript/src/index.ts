838197d564a4f1ebe07c6a160a6fe1617600d4a3
diwakarsrd28-design <diwakarsrd28@gmail.com>
2026-08-25 21:20:17 +0530

    feat: implement index.ts module

---
+++ sdk/typescript/src/index.ts
@@ -0,0 +1,16 @@
+export { ContextEngine } from "./client.js";
+export type { ContextEngineOptions } from "./client.js";
+export { ContextFlowError } from "./errors.js";
+export type {
+  ContextObjectResponse,
+  ContextPackResponse,
+  ContextPackOptions,
+  ContextTraceResponse,
+  ConflictingClaim,
+  MemoryEntryResponse,
+  MemoryScope,
+  RecallOptions,
+  SearchOptions,
+  TraceOptions,
+  TraceStage,
+} from "./types.js";
